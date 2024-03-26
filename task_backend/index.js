38dad688e6b5571458ebb3d0371b6055a337e7f0
MOHD ADIL <119355639+mohdadil12345@users.noreply.github.com>
2024-03-27 01:16:10 +0530

    task route along with auth middleware added

---
+++ task_backend/index.js
@@ -0,0 +1,31 @@
+
+
+const express = require("express")
+require("dotenv").config()
+const cors = require("cors")
+
+const { userRouter } = require("./routes/user.routes")
+const { connection } = require("./db")
+
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+
+app.use("/user", userRouter)
+
+
+
+app.listen(process.env.port, async() => {
+   try{
+
+    await connection
+    console.log("connected to db")
+    console.log(`port running at ${process.env.port}`)
+
+   }catch(err){
+    console.log(err)
+   }
+})
+
